f848fa4da504451842e7ad9d465c46df1feee62b
napeng <napeng@siasun.com>
2018-04-24 21:36:33 +0800

    更换正版dev

--- 代码/WMS/应用/WMS_Main/MainFrame.designer.cs
+++ 代码/WMS/应用/WMS_Main/MainFrame.designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrame));
             this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
             this.ribbon_Title = new DevExpress.XtraBars.Ribbon.RibbonControl();
@@ -54,9 +55,9 @@
             this.barStaticItem2 = new DevExpress.XtraBars.BarStaticItem();
             this.barStaticItem3 = new DevExpress.XtraBars.BarStaticItem();
             this.barStaticItem4 = new DevExpress.XtraBars.BarStaticItem();
-            this.xtraTabbedMdiManager1 = new DevExpress.XtraTabbedMdi.XtraTabbedMdiManager();
-            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel();
-            this.dockManager1 = new DevExpress.XtraBars.Docking.DockManager();
+            this.xtraTabbedMdiManager1 = new DevExpress.XtraTabbedMdi.XtraTabbedMdiManager(this.components);
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.dockManager1 = new DevExpress.XtraBars.Docking.DockManager(this.components);
             this.dockPanel1 = new DevExpress.XtraBars.Docking.DockPanel();
             this.dockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
             this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
@@ -78,7 +79,7 @@
             // 
             this.ribbon_Title.AllowMdiChildButtons = false;
             this.ribbon_Title.AllowMinimizeRibbon = false;
-            this.ribbon_Title.ApplicationCaption = "213123";
+            this.ribbon_Title.ApplicationCaption = "WMS";
             this.ribbon_Title.ApplicationIcon = ((System.Drawing.Bitmap)(resources.GetObject("ribbon_Title.ApplicationIcon")));
             this.ribbon_Title.DrawGroupCaptions = DevExpress.Utils.DefaultBoolean.True;
             this.ribbon_Title.ExpandCollapseItem.Id = 0;
@@ -397,7 +398,7 @@
             this.RibbonVisibility = DevExpress.XtraBars.Ribbon.RibbonVisibility.Visible;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.StatusBar = this.ribbonStatusBar;
-            this.Text = "213123";
+            this.Text = "WMS";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainView_FormClosing);
             this.Load += new System.EventHandler(this.MainView_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ribbon_Title)).EndInit();
