5814e17d95b7be7f3b40e75b093bc4a06761c63c
napeng <napeng@siasun.com>
2018-05-05 11:33:34 +0800

    增加漏传文件，及数据库，主要增加货位配置

---
+++ 代码/WMS/立库管理/WMS_View/View/CellSetView.designer.cs
@@ -0,0 +1,371 @@
+﻿namespace WMS_Kernel
+{
+    partial class CellSetView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.cb_ColListSTArea = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_HouseArea = new System.Windows.Forms.ComboBox();
+            this.bt_AreaSet = new System.Windows.Forms.Button();
+            this.cb_ColListEDArea = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_startLayer = new System.Windows.Forms.ComboBox();
+            this.cb_EndLayer = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cb_LayerList = new System.Windows.Forms.ComboBox();
+            this.bt_GsFobit = new System.Windows.Forms.Button();
+            this.rb_SingleLayer = new System.Windows.Forms.RadioButton();
+            this.bt_UseGs = new System.Windows.Forms.Button();
+            this.rb_singleCol = new System.Windows.Forms.RadioButton();
+            this.cb_ColSTList = new System.Windows.Forms.ComboBox();
+            this.cb_ColEDList = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb_ColListSTArea
+            // 
+            this.cb_ColListSTArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_ColListSTArea.FormattingEnabled = true;
+            this.cb_ColListSTArea.Location = new System.Drawing.Point(112, 29);
+            this.cb_ColListSTArea.Name = "cb_ColListSTArea";
+            this.cb_ColListSTArea.Size = new System.Drawing.Size(59, 22);
+            this.cb_ColListSTArea.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 14);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "货位逻辑库区";
+            // 
+            // cb_HouseArea
+            // 
+            this.cb_HouseArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_HouseArea.FormattingEnabled = true;
+            this.cb_HouseArea.Location = new System.Drawing.Point(132, 118);
+            this.cb_HouseArea.Name = "cb_HouseArea";
+            this.cb_HouseArea.Size = new System.Drawing.Size(231, 22);
+            this.cb_HouseArea.TabIndex = 13;
+            // 
+            // bt_AreaSet
+            // 
+            this.bt_AreaSet.Location = new System.Drawing.Point(278, 154);
+            this.bt_AreaSet.Name = "bt_AreaSet";
+            this.bt_AreaSet.Size = new System.Drawing.Size(79, 31);
+            this.bt_AreaSet.TabIndex = 9;
+            this.bt_AreaSet.Text = "设定";
+            this.bt_AreaSet.UseVisualStyleBackColor = true;
+            this.bt_AreaSet.Click += new System.EventHandler(this.bt_AreaSet_Click);
+            // 
+            // cb_ColListEDArea
+            // 
+            this.cb_ColListEDArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_ColListEDArea.FormattingEnabled = true;
+            this.cb_ColListEDArea.Location = new System.Drawing.Point(267, 29);
+            this.cb_ColListEDArea.Name = "cb_ColListEDArea";
+            this.cb_ColListEDArea.Size = new System.Drawing.Size(67, 22);
+            this.cb_ColListEDArea.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 14);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "起始列";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 14);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "终止列";
+            // 
+            // cb_startLayer
+            // 
+            this.cb_startLayer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_startLayer.FormattingEnabled = true;
+            this.cb_startLayer.Location = new System.Drawing.Point(112, 59);
+            this.cb_startLayer.Name = "cb_startLayer";
+            this.cb_startLayer.Size = new System.Drawing.Size(59, 22);
+            this.cb_startLayer.TabIndex = 17;
+            // 
+            // cb_EndLayer
+            // 
+            this.cb_EndLayer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_EndLayer.FormattingEnabled = true;
+            this.cb_EndLayer.Location = new System.Drawing.Point(267, 59);
+            this.cb_EndLayer.Name = "cb_EndLayer";
+            this.cb_EndLayer.Size = new System.Drawing.Size(67, 22);
+            this.cb_EndLayer.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(61, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 14);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "起始层";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(216, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 14);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "终止层";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label8.Location = new System.Drawing.Point(23, 93);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(344, 3);
+            this.label8.TabIndex = 21;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.cb_EndLayer);
+            this.groupBox2.Controls.Add(this.cb_startLayer);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.cb_ColListEDArea);
+            this.groupBox2.Controls.Add(this.bt_AreaSet);
+            this.groupBox2.Controls.Add(this.cb_HouseArea);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.cb_ColListSTArea);
+            this.groupBox2.Location = new System.Drawing.Point(12, 185);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(381, 192);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "逻辑库区设置";
+            // 
+            // cb_LayerList
+            // 
+            this.cb_LayerList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_LayerList.FormattingEnabled = true;
+            this.cb_LayerList.Location = new System.Drawing.Point(125, 62);
+            this.cb_LayerList.Name = "cb_LayerList";
+            this.cb_LayerList.Size = new System.Drawing.Size(231, 22);
+            this.cb_LayerList.TabIndex = 5;
+            // 
+            // bt_GsFobit
+            // 
+            this.bt_GsFobit.Location = new System.Drawing.Point(281, 92);
+            this.bt_GsFobit.Name = "bt_GsFobit";
+            this.bt_GsFobit.Size = new System.Drawing.Size(70, 31);
+            this.bt_GsFobit.TabIndex = 3;
+            this.bt_GsFobit.Text = "禁用";
+            this.bt_GsFobit.UseVisualStyleBackColor = true;
+            this.bt_GsFobit.Click += new System.EventHandler(this.bt_GsFobit_Click);
+            // 
+            // rb_SingleLayer
+            // 
+            this.rb_SingleLayer.AutoSize = true;
+            this.rb_SingleLayer.Location = new System.Drawing.Point(23, 62);
+            this.rb_SingleLayer.Name = "rb_SingleLayer";
+            this.rb_SingleLayer.Size = new System.Drawing.Size(49, 18);
+            this.rb_SingleLayer.TabIndex = 12;
+            this.rb_SingleLayer.Text = "单层";
+            this.rb_SingleLayer.UseVisualStyleBackColor = true;
+            // 
+            // bt_UseGs
+            // 
+            this.bt_UseGs.Location = new System.Drawing.Point(204, 92);
+            this.bt_UseGs.Name = "bt_UseGs";
+            this.bt_UseGs.Size = new System.Drawing.Size(70, 31);
+            this.bt_UseGs.TabIndex = 9;
+            this.bt_UseGs.Text = "启用";
+            this.bt_UseGs.UseVisualStyleBackColor = true;
+            this.bt_UseGs.Click += new System.EventHandler(this.bt_UseGs_Click);
+            // 
+            // rb_singleCol
+            // 
+            this.rb_singleCol.AutoSize = true;
+            this.rb_singleCol.Checked = true;
+            this.rb_singleCol.Location = new System.Drawing.Point(23, 35);
+            this.rb_singleCol.Name = "rb_singleCol";
+            this.rb_singleCol.Size = new System.Drawing.Size(49, 18);
+            this.rb_singleCol.TabIndex = 11;
+            this.rb_singleCol.TabStop = true;
+            this.rb_singleCol.Text = "单列";
+            this.rb_singleCol.UseVisualStyleBackColor = true;
+            // 
+            // cb_ColSTList
+            // 
+            this.cb_ColSTList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_ColSTList.FormattingEnabled = true;
+            this.cb_ColSTList.Location = new System.Drawing.Point(177, 29);
+            this.cb_ColSTList.Name = "cb_ColSTList";
+            this.cb_ColSTList.Size = new System.Drawing.Size(59, 22);
+            this.cb_ColSTList.TabIndex = 3;
+            // 
+            // cb_ColEDList
+            // 
+            this.cb_ColEDList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_ColEDList.FormattingEnabled = true;
+            this.cb_ColEDList.Location = new System.Drawing.Point(296, 29);
+            this.cb_ColEDList.Name = "cb_ColEDList";
+            this.cb_ColEDList.Size = new System.Drawing.Size(59, 22);
+            this.cb_ColEDList.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 14);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "起始列";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(243, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 14);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "终止列";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.cb_ColEDList);
+            this.groupBox3.Controls.Add(this.cb_ColSTList);
+            this.groupBox3.Controls.Add(this.rb_singleCol);
+            this.groupBox3.Controls.Add(this.bt_UseGs);
+            this.groupBox3.Controls.Add(this.rb_SingleLayer);
+            this.groupBox3.Controls.Add(this.bt_GsFobit);
+            this.groupBox3.Controls.Add(this.cb_LayerList);
+            this.groupBox3.Location = new System.Drawing.Point(12, 51);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(381, 131);
+            this.groupBox3.TabIndex = 16;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "货位启禁用设置";
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.AllowKeyTips = false;
+            this.ribbonControl1.AllowMdiChildButtons = false;
+            this.ribbonControl1.AllowMinimizeRibbon = false;
+            this.ribbonControl1.AllowTrimPageText = false;
+            this.ribbonControl1.CausesValidation = false;
+            this.ribbonControl1.DrawGroupsBorder = false;
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 1;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl1.ShowCategoryInCaption = false;
+            this.ribbonControl1.ShowDisplayOptionsMenuButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl1.ShowExpandCollapseButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl1.ShowPageHeadersInFormCaption = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbonControl1.ShowPageHeadersMode = DevExpress.XtraBars.Ribbon.ShowPageHeadersMode.Hide;
+            this.ribbonControl1.ShowQatLocationSelector = false;
+            this.ribbonControl1.ShowToolbarCustomizeItem = false;
+            this.ribbonControl1.Size = new System.Drawing.Size(412, 33);
+            this.ribbonControl1.Toolbar.ShowCustomizeItem = false;
+            // 
+            // CellSetView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 385);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.ribbonControl1);
+            this.MaximizeBox = false;
+            this.Name = "CellSetView";
+            this.Ribbon = this.ribbonControl1;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "货位设置";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ComboBox cb_ColListSTArea;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_HouseArea;
+        private System.Windows.Forms.Button bt_AreaSet;
+        private System.Windows.Forms.ComboBox cb_ColListEDArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cb_startLayer;
+        private System.Windows.Forms.ComboBox cb_EndLayer;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cb_LayerList;
+        private System.Windows.Forms.Button bt_GsFobit;
+        private System.Windows.Forms.RadioButton rb_SingleLayer;
+        private System.Windows.Forms.Button bt_UseGs;
+        private System.Windows.Forms.RadioButton rb_singleCol;
+        private System.Windows.Forms.ComboBox cb_ColSTList;
+        private System.Windows.Forms.ComboBox cb_ColEDList;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+    }
+}
